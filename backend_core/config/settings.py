ce8499d1ae5553710f557b6476565aa5af5e273a
Anna Gorkiewicz <an.gorkiewicz@gmail.com>
2023-02-25 02:03:36 +0100

    install simplejwt and initial configuration

--- backend_core/config/settings.py
+++ backend_core/config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework_simplejwt',
     'drf_spectacular',
 
     'users.apps.UsersConfig',
@@ -147,6 +148,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
