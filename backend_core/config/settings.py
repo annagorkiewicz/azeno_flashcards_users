7fdfbc86a666ac0b678ca5ab5e62a31c967c0f40
Anna Gorkiewicz <an.gorkiewicz@gmail.com>
2023-03-11 19:44:43 +0100

    adjused settings

--- backend_core/config/settings.py
+++ backend_core/config/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', None)
+SECRET_KEY = os.environ.get('DJ_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DJ_DEBUG', 0))
 
-ALLOWED_HOSTS = ['0.0.0.0', 'backend_core']
+ALLOWED_HOSTS = os.environ.get('DJ_ALLOWED_HOSTS', '').split()
 
 # Application definition
 
